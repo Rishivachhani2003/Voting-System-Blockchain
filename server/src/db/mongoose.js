31515a07e4cbd8a1277c6b4a86cdc3e93f0f9060
Rishivachhani2003 <vachhanirishi2003@gmail.com>
2025-04-05 11:43:58 +0530

    after dockerising the project and after adding the cloud database

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const conn = mongoose.connect('mongodb://localhost:27017/elections',{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+// const conn = mongoose.connect('mongodb://localhost:27017/elections',{
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// })
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const conn = mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
